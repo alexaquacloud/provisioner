86f47d0c8ba50c92f43a19371cba23d30525d363
Alex Netrebskii <alex@aqua-cloud.io>
2021-08-20 10:31:22 +0400

    Update file.js

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 function a() {
  var test = 1 + 1;
  var test2 = 1 + 3;
- authenticate("alex", "fakepassword123SS")
+ authenticate("alex", "fakepassword123SS");
 }
 
 function authenticate(username, password) {
